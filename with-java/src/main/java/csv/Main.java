ceac99f6b147c5663a5562a61406c4e86feb8a49
k-oketa <oketean@gmail.com>
2020-11-09 17:52:24 +0900

    [add] 2020年度の完成版を追加

---
+++ with-java/src/main/java/csv/Main.java
@@ -0,0 +1,9 @@
+package csv;
+
+public class Main {
+    public static void main(String[] args) {
+        var users = new UserCsvReader().readUserCsv("users.csv");
+        // TODO Q2.1
+
+    }
+}
---
+++ with-java/src/test/java/passphrase/PassphraseGeneratorTests.java
@@ -0,0 +1,17 @@
+package passphrase;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PassphraseGeneratorTests {
+
+    private final List<String> lowers;
+    private final List<String> uppers;
+    private final List<String> numbers;
+
+    PassphraseGeneratorTests() {
+        this.lowers = Arrays.asList("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(","));
+        this.uppers = Arrays.asList("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(","));
+        this.numbers = Arrays.asList("1,2,3,4,5,6,7,8,9,0".split(","));
+    }
+}
